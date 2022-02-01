942cb28471e33ca5e6dbc8a3ab7fcd02958ce353
EJamdar05 <jamdareshaq@gmail.com>
2022-01-31 16:19:00 -0800

    added splash screen

--- app/src/main/java/com/ejamdar/flixapp/MovieAdapter.kt
+++ app/src/main/java/com/ejamdar/flixapp/MovieAdapter.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
