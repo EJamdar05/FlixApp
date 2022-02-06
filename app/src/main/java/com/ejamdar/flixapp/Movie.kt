1877e312fb06b78e8b18336a654345d9c02846b6
EJamdar05 <jamdareshaq@gmail.com>
2022-02-06 13:59:39 -0800

    added clickable scenes and ui changes

--- app/src/main/java/com/ejamdar/flixapp/Movie.kt
+++ app/src/main/java/com/ejamdar/flixapp/Movie.kt
@@ -1,13 +1,19 @@
 package com.ejamdar.flixapp
 
+import android.os.Parcelable
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie (
     val movieID: Int,
     private val posterPath: String,
     val title: String,
     val overview: String,
-){
+    val rating:Double,
+): Parcelable{
+    @IgnoredOnParcel
     val baseURL = "https://image.tmdb.org/t/p/w342/$posterPath"
     companion object{
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie>{
@@ -19,8 +25,8 @@ data class Movie (
                         movieJson.getInt("id"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
-                        movieJson.getString("overview")
-
+                        movieJson.getString("overview"),
+                        movieJson.getDouble("vote_average")
                     )
                 )
             }
--- app/src/main/java/com/ejamdar/flixapp/MovieAdapter.kt
+++ app/src/main/java/com/ejamdar/flixapp/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.ejamdar.flixapp
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,13 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 private const val TAG = "MovieAdapter"
+ const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -33,14 +37,35 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         return movies.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOver)
         private val tvPoster = itemView.findViewById<ImageView>(R.id.tvPoster)
-        fun bind(movie: Movie){
+
+        init{
+            itemView.setOnClickListener(this)
+        }
+        fun bind(movie: Movie){ //logic of click listener
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
-            Glide.with(context).load(movie.baseURL).into(tvPoster)
+            //Glide.with(context).load(movie.baseURL).into(tvPoster)
+            val radius = 30; // corner radius, higher value = more rounded
+            val margin = 10; // crop margin, set to 0 for corners with no crop
+            Glide.with(context)
+                .load(movie.baseURL)
+                .transform(RoundedCornersTransformation(radius, margin))
+                .into(tvPoster)
+
+        }
+
+        override fun onClick(p0: View?) {
+            //notify movie tapped on
+            val selectedMovie = movies[adapterPosition]
+            //intent sys to switch to new scene
+            val intent = Intent(context, MovieDetails::class.java)
+            intent.putExtra(MOVIE_EXTRA, selectedMovie) //passing entire object
+            context.startActivity(intent)
+
         }
     }
 
